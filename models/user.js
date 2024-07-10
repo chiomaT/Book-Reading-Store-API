a3b145d0acfde55f4b245fd99ae780405a3d4147
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-07-10 12:48:57 +0100

    full name on request body

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  full_name: { type: String, required: true},
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
