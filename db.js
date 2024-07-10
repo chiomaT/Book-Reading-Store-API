aae5b1729dd1aa7b4517d2ebad69b87fecc215d1
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-07-10 10:41:55 +0100

    Error handling on DB connection

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
-import mongoose from 'mongoose';
-const connectDB = async () => {
+import mongoose from "mongoose";
+const connectDB = async (retries = 5, delay = 5000) => {
+  while (retries) {
     try {
       await mongoose.connect(process.env.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       });
+      console.log("Connected to th DB");
+      break;
     } catch (error) {
-        console.error(err.message);
+      console.error("Database connection failed");
+      console.error(`Error: ${error.message}`);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      if (retries === 0) {
+        console.error("No more retries left. Exiting now...");
         process.exit(1);
+      }
+      await new Promise((res) => setTimeout(res, delay));
     }
-  };
+  }
+};
 export default connectDB;
